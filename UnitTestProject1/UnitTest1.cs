efe9002252c39519c0d1972d3a9f0312241d7f6f
The-Eugenef <rodneyhalo@gmail.com>
2022-06-27 15:20:57 +0600

    хз заебался

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using шалавы;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 5, b = 6, count = 1;
+            Form1 f = new Form1();
+            void
+          
+        }
+    }
+}
