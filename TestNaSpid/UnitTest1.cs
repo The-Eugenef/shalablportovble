2b3415295afdc3cb3ba9fdaccfec5a437e4e2093
user <user@SH1GAAA>
2022-06-28 21:03:33 +0600

    booba

--- TestNaSpid/UnitTest1.cs
+++ TestNaSpid/UnitTest1.cs
@@ -11,9 +11,10 @@ namespace TestNaSpid.Tests
         public void Skladuvaet()
         {
             float x = 47.3F, y = 84.12F;
+            string c = "+";
             float expected = x + y;
             Form1 TEST = new Form1();
-            float actual = TEST.MathClass.Addition(x, y);
+            float actual = TEST.MathClass.Calculate(x, y, c);
             Assert.AreEqual(expected, actual, "PEREDELAI!!!");
         }
 
@@ -21,9 +22,10 @@ namespace TestNaSpid.Tests
         public void Otnimaet()
         {
             float x = 216.97F, y = 81.28F;
+            string c = "-";
             float expected = x - y;
             Form1 TEST = new Form1();
-            float actual = TEST.MathClass.Subtraction(x, y);
+            float actual = TEST.MathClass.Calculate(x, y, c);
             Assert.AreEqual(expected, actual, "PEREDELAI!!!");
         }
 
@@ -31,9 +33,10 @@ namespace TestNaSpid.Tests
         public void Umnozhaet()
         {
             float x = 123F, y = 321F;
+            string c = "*";
             float expected = x * y;
             Form1 TEST = new Form1();
-            float actual = TEST.MathClass.Multiplication(x, y);
+            float actual = TEST.MathClass.Calculate(x, y, c);
             Assert.AreEqual(expected, actual, "PEREDELAI!!!");
         }
 
@@ -41,9 +44,10 @@ namespace TestNaSpid.Tests
         public void Delit()
         {
             float x = 999.99F, y = 111.11F;
+            string c = "/";
             float expected = x / y;
             Form1 TEST = new Form1();
-            float actual = TEST.MathClass.Division(x, y);
+            float actual = TEST.MathClass.Calculate(x, y, c);
             Assert.AreEqual(expected, actual, "PEREDELAI!!!");
         }
 
@@ -51,19 +55,21 @@ namespace TestNaSpid.Tests
         public void Stepenit()
         {
             float x = 11F, y = 4F;
+            string c = "^";
             float expected = (float)Math.Pow(x, y);
             Form1 TEST = new Form1();
-            float actual = TEST.MathClass.FuncPow(x, y);
+            float actual = TEST.MathClass.Calculate(x, y, c);
             Assert.AreEqual(expected, actual, "PEREDELAI!!!");
         }
 
         [TestMethod]
         public void Exponiruet()
         {
-            float x = 5F;
+            float x = 5F, y=0;
+            string c = "e^";
             float expected = (float)Math.Exp(x);
             Form1 TEST = new Form1();
-            float actual = TEST.MathClass.Exponent(x);
+            float actual = TEST.MathClass.Calculate(x, y, c);
             Assert.AreEqual(expected, actual, "PEREDELAI!!!");
         }
     }
